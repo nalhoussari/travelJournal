6b846c900a61679d26e5d763c8bbc2d968394eaa
Katrina de Guzman <katrinavdeguzman@gmail.com>
2017-07-13 13:54:33 -0700

    Create tableviewcell files

---
+++ travelJournal/MyEntriesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyEntriesTableViewCell.swift
+//  travelJournal
+//
+//  Created by Katrina de Guzman on 2017-07-13.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+import UIKit
+
+class MyEntriesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
