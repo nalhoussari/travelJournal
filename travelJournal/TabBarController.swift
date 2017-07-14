45668e4e32549f454bd837884045740965bafa6f
nalhoussari <nalhoussari@hotmail.com>
2017-07-14 12:24:33 -0700

    Adding the plus button and TabBarController file

---
+++ travelJournal/TabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  TabBarController.swift
+//  travelJournal
+//
+//  Created by Noor Alhoussari on 2017-07-14.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(TabBarController.myRightSideBarButtonItemTapped(_:)))
+        
+        self.navigationItem.rightBarButtonItem = addButton
+        
+    }
+    
+    func myRightSideBarButtonItemTapped(_ sender:UIBarButtonItem!) {
+        print("myRightSideBarButtonItemTapped")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    
+}
+
