b12105fe4adbfc58655f42fdea39c742fe41fd70
Katrina de Guzman <katrinavdeguzman@gmail.com>
2017-07-20 19:01:51 -0700

    Populate map with pins

--- travelJournal/MapViewController.swift
+++ travelJournal/MapViewController.swift
@@ -12,66 +12,41 @@ import CoreLocation
 
 
 class MapViewController: UIViewController {
+    
     //MARK: - Properties
-    @IBOutlet weak var mapView: MKMapView!//map
+    var entries = [JournalModel]()
+    
+    @IBOutlet weak var mapView: MKMapView!
     let regionRadius : CLLocationDistance = 1000
-    // MARK: - View Life Cycle
+    var annotations : [MKPointAnnotation]=[]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let initialLocation = CLLocation(latitude: 49.2819, longitude: -123.1083)//map
-        centerMapOnLocation(initialLocation)//map
-        
-        mapView.mapType = MKMapType.hybrid//map
+        mapView.mapType = MKMapType.hybrid
+        let initialLocation = CLLocation(latitude: 49.2819, longitude: -123.1083)
+        centerMapOnLocation(initialLocation)
+        getRecords()
     }
-//    // MARK: - Actions
-//    @IBAction func geocode(_ sender: UIButton) {
-//        guard let street = streetTextField.text else { return }
-//        
-//        // Geocode Address String
-//        let address = "\(street)"
-//        geocoder.geocodeAddressString(address) { (placemarks, error) in
-//            self.processResponse(withPlacemarks: placemarks, error: error)
-//        }
-//        
-//        // Update View
-//        geocodeButton.isHidden = true
-//        activityIndicatorView.startAnimating()
-//    }
-//    
-//    // MARK: - Helper Methods
-//    private func processResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
-//        // Update View
-//        geocodeButton.isHidden = false
-//        activityIndicatorView.stopAnimating()
-//        
-//        if let error = error {
-//            print("Unable to Forward Geocode Address (\(error))")
-//            locationLabel.text = "Unable to Find Location for Address"
-//            
-//        } else {
-//            var location: CLLocation?
-//            
-//            if let placemarks = placemarks, placemarks.count > 0 {
-//                location = placemarks.first?.location
-//            }
-//            
-//            if let location = location {
-//                let coordinate = location.coordinate
-//                locationLabel.text = "\(coordinate.latitude), \(coordinate.longitude)"
-////                annotation.coordinate = location.coordinate//map
-////                mapView.addAnnotation(annotation)//map
-////                centerMapOnLocation(CLLocation(latitude:location.coordinate.latitude, longitude:location.coordinate.longitude)) //map
-//            } else {
-//                locationLabel.text = "No Matching Location Found"
-//            }
-//        }
-//    }
     
     func centerMapOnLocation(_ location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                                                                  regionRadius * 3.0, regionRadius * 3.0)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 3.0, regionRadius * 3.0)
         mapView.setRegion(coordinateRegion, animated: true)
-    }//map
+    }
+    
+    func getRecords(){
+        FBDatabase.GetJournalsFromDatabase { (journalArray) in
+            self.entries = journalArray
+            self.pinEntries()
+        }
+    }
+    func pinEntries(){
+        for entryTemp in (self.entries){
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2DMake(CLLocationDegrees(entryTemp.latitude), CLLocationDegrees(entryTemp.longitude))
+            annotations.append(annotation)
+        }
+        mapView.addAnnotations(annotations)
+
+    }
     
 }
